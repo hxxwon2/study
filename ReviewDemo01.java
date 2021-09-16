3bb824929f3c9b94ab8ecfe4b83db441b0bcd36d
hxxwon2 <jo9770@naver.com>
2021-09-16 20:51:27 +0900

    v0.6

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-//v0.5
+//v0.6
 public class ReviewDemo01 {
     public static void main(String[] args) {
         //소수 :1과 자기자신외에는 나누어 떨어지지 않는 수
@@ -8,18 +8,14 @@ public class ReviewDemo01 {
         boolean isPrime = true;
 
         int k=2;
-        while(k<number) {
-            if (number % k == 0) {
+        while(k<number){
+            if(number % k ==0){
                 isPrime = false;
-                break; //첫 번째 약수가 발견되면 for문 탈출(입력한 숫자가 소수일때는 영향x)
+                break;//첫 번째 약수가 발견되면 for문 탈출(입력한 숫자가 소수일때는 영향x)
             }
-            //System.out.println(k);
             k++;
         }
 
-        if (isPrime)   //isPrime 변수 값이 true면! (isPrime==true를 의미)
-            System.out.println(number + "은(는) 소수!");
-        else
-            System.out.println(number + "은(는) 소수가 아닙니다.");
+        System.out.println(number + (isPrime ? "은(는) 소수!" : "은(는) 소수가 아닙니다."));
     }
 }
