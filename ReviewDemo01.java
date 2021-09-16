6f9fba77dc68c238aa8f196dae94bad659a7e7d6
hxxwon2 <jo9770@naver.com>
2021-09-16 20:47:28 +0900

    v0.5

--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
-//v0.3
+//v0.5
 public class ReviewDemo01 {
     public static void main(String[] args) {
         //소수 :1과 자기자신외에는 나누어 떨어지지 않는 수
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        //int cnt = 0;
         boolean isPrime = true;
 
-        for (int k=2; k<number; k++) { //반복 횟수 2회 감소
-            if (number % k == 0) //입력된 수를 k로 나누었을 때 나머지가 0인경우.즉, 약수
-                //cnt++;//cnt=cnt+1
+        int k=2;
+        while(k<number) {
+            if (number % k == 0) {
                 isPrime = false;
+                break; //첫 번째 약수가 발견되면 for문 탈출(입력한 숫자가 소수일때는 영향x)
+            }
+            //System.out.println(k);
+            k++;
         }
 
-        if (isPrime)   //isPrime 변수 값이 true면 (isPrime==true를 의미)
+        if (isPrime)   //isPrime 변수 값이 true면! (isPrime==true를 의미)
             System.out.println(number + "은(는) 소수!");
         else
             System.out.println(number + "은(는) 소수가 아닙니다.");
